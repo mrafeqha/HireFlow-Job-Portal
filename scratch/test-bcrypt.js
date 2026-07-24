4301c6597ce6759bd737b87b9b9ac1e4d2971e9a
mrafeqha <mrafeqha@gmail.com>
2026-07-24 21:58:58 +0530

    Remove temporary test files

--- scratch/test-bcrypt.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2a$10$63vB8599z9Ld56Q8qZ.qeeN9.hO5.kOq7K9cQ65F6t8sS75aU8YV2';
-
-console.log('Seeded Hash matches password123:', bcrypt.compareSync('password123', hash));
-
-const freshHash = bcrypt.hashSync('password123', 10);
-console.log('Freshly generated hash for password123:', freshHash);
